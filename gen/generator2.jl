3917edd3fea04883bd194b9d3a4706bd277ad0bc
Ashwani Rathee <ab669522@gmail.com>
2022-08-22 14:43:07 +0530

    generator fixes

--- gen/generator2.jl
+++ gen/generator2.jl
@@ -2,12 +2,8 @@ using Clang.JLLEnvs
 using Clang.Generators
 using libopenjpegextra_jll
 
-
 include_dir = normpath(libopenjpegextra_jll.artifact_dir, "include")
-include_dir2 = normpath(OpenJpeg_jll.artifact_dir, "include/openjpeg-2.4")
-headers2 = [joinpath(include_dir, header) for header in readdir(include_dir) if endswith(header, ".h")]
-headers = [joinpath(include_dir2, header) for header in readdir(include_dir2) if endswith(header, ".h")]
-append!(headers, headers2)
+headers = [joinpath(include_dir, header) for header in readdir(include_dir) if endswith(header, ".h")]
 options = load_options(joinpath(@__DIR__, "generator2.toml"))
 
 args =   get_default_args()
